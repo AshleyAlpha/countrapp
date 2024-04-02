b274248216c101ec9abba57f19b404f227dbfd7f
AshleyAlpha <mashleyalpha@gmail.com>
2024-04-02 06:16:09 -0700

    moved the footer section at the bottom

--- countapp/src/components/footer.js
+++ countapp/src/components/footer.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-        <div className=" bg-slate-50 h-24 w-full flex dm:flex-row sm:flex-row flex-col sm:justify-around md:justify-around sm:items-center md:items-center">
+    <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+    <div className="bg-slate-50 h-24 w-full flex dm:flex-row sm:flex-row flex-col sm:justify-around md:justify-around sm:items-center md:items-center">
         <h1 className="text-4xl text-blue-600 font-bold">Logoipsum</h1>
-        <p className='text-gray-600 mt-4'>Copyright © 2024. All rights reserved.</p>
-        </div>
+        <p className="text-gray-600 mt-4">Copyright © 2024. All rights reserved.</p>
     </div>
+</div>
+
   )
 }
