750c78d485860db06f0eb86aa3796dd604cedafd
AshleyAlpha <mashleyalpha@gmail.com>
2024-04-04 02:10:03 -0700

    imported footer section

--- countapp/src/App.js
+++ countapp/src/App.js
@@ -1,15 +1,21 @@
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
-import { Home } from "./components/home";
+import { Home } from "../src/home";
+import { Routes, Route } from "react-router-dom";
+import { About } from "./components/pages/about";
+import './App.css'
+
 export default function App() {
   return (
     <div>
-    <Header/>
-    <Footer/>
-    <Home/>
-  </div>
+      <div class="mesure">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
-
-
-
--- countapp/src/components/footer.js
+++ countapp/src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+    <div style={{ width: "100%" }}>
     <div className="bg-slate-50 h-24 w-full flex dm:flex-row sm:flex-row flex-col sm:justify-around md:justify-around sm:items-center md:items-center">
         <h1 className="text-4xl text-blue-600 font-bold">Logoipsum</h1>
         <p className="text-gray-600 mt-4">Copyright © 2024. All rights reserved.</p>
